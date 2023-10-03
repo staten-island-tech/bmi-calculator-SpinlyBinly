e1b5950ef050cc65b7bc23b479eaff18d9952f75
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-10-03 13:09:14 -0400

    commit 1

---
+++ index.js
@@ -0,0 +1,11 @@
+let variable; //variable declaration
+variable = "variable";
+let number = 7;
+let string = "Hello";
+let bigstring = "hello2";
+var variable2 = "var";
+let variable3;
+variable = "nope";
+console.log(variable);
+const constant = "constant";
+constant = "shkashdj";
